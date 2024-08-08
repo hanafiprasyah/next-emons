3f40377d2436cdacd7525ee4f872c918d222a6a7
Muhammad Hanafi Prasyah <74239181+hanafiprasyah@users.noreply.github.com>
2024-08-08 13:51:04 +0700

    pref(core): Update packages and improve the app default Header

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,13 @@ const nextConfig = {
   reactStrictMode: false,
   optimizeFonts: true,
   images: {
+    formats: ["image/webp", "image/avif"],
     remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "*.emons.id",
+        port: "",
+      },
       {
         protocol: "https",
         hostname: "*.unsplash.com",
@@ -16,6 +22,39 @@ const nextConfig = {
         port: "",
       },
     ],
+  },
+  async headers() {
+    return [
+      {
+        source: "/api/(.*)",
+        headers: [
+          {
+            key: "X-API-Version",
+            value: "1.0.0",
+          },
+          {
+            key: "Content-Security-Policy",
+            value: "default-src 'self'; script-src 'self'; object-src 'none';",
+          },
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+          {
+            key: "X-Frame-Options",
+            value: "DENY",
+          },
+          {
+            key: "X-XSS-Protection",
+            value: "1; mode=block",
+          },
+          {
+            key: "Cache-Control",
+            value: "s-maxage=10",
+          },
+        ],
+      },
+    ];
   },
 };
 
