58074fba43e7e3808156c119d8f40584140526f7
Muhammad Hanafi Prasyah <74239181+hanafiprasyah@users.noreply.github.com>
2024-08-15 09:56:08 +0700

    feat(api): Using realtime REST API for voltage monitoring

--- pages/api/monitoring/voltage/getdata.js
+++ pages/api/monitoring/voltage/getdata.js
@@ -21,7 +21,7 @@ export default async function handler(req, res) {
 
   try {
     const response = await fetch(
-      `http://45.13.132.175:8802/device/getdatavoltage`,
+      `http://45.13.132.175:8802/device/getlastdatavoltage`,
       {
         method: "POST",
         headers: {
