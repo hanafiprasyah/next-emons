95011ea4488f59d068e3c726f337193d22eaa7c6
Muhammad Hanafi Prasyah <74239181+hanafiprasyah@users.noreply.github.com>
2024-09-20 14:46:00 +0700

    pref(core): new API to get all last data monitoring and Cache re-Configuration

--- app/components/PrelineScript.tsx
+++ app/components/PrelineScript.tsx
@@ -1,29 +1,26 @@
 'use client'
 
-import { usePathname } from "next/navigation";
-import { useEffect } from "react";
+import { usePathname } from 'next/navigation'
+import { useEffect } from 'react'
 
-import { IStaticMethods } from "preline/preline";
+import { IStaticMethods } from 'preline/preline'
+
 declare global {
     interface Window {
-        HSStaticMethods: IStaticMethods;
+        HSStaticMethods: IStaticMethods
     }
 }
 
 export default function PrelineScript() {
-    const path = usePathname();
+    const path = usePathname()
 
     useEffect(() => {
         const loadPreline = async () => {
-            await import("preline/preline");
-            path?.endsWith('/') ? window.HSStaticMethods.autoInit() : window.HSStaticMethods.autoInit();
-            if (process.env.NODE_ENV === 'development') {
-                console.log('--- Preline initialized ---')
-            }
-        };
+            await import('preline/preline')
+            window.HSStaticMethods.autoInit()
+        }
+        loadPreline()
+    }, [path])
 
-        loadPreline();
-    }, [path]);
-
-    return null;
+    return null
 }
--- pages/api/monitoring/getmonitoring.js
+++ pages/api/monitoring/getmonitoring.js
@@ -1,8 +1,5 @@
 "use server";
 
-import { NextResponse, NextRequest } from "next/server";
-import { encryptCookies } from "@/lib/helper/cookie-encryption";
-
 export default async function handler(req, res) {
   if (req.method !== "POST") {
     if (process.env.NODE_ENV === "development") {
@@ -11,14 +8,20 @@ export default async function handler(req, res) {
     throw new Error("Error 405");
   }
 
-  const { tenant, userName, password, salt } = JSON.parse(req.body);
-  const oneDay = 24 * 60 * 60 * 1000;
-  const secretKey = process.env.CRYPT_SECRET;
-  const encryptedCookieString = encryptCookies(tenant, secretKey);
+  const {
+    locationid,
+    lane,
+    status,
+    value,
+    side,
+    start_trancation_date,
+    end_trancation_date,
+    tenant,
+  } = req.body;
 
   try {
     const response = await fetch(
-      `${process.env.BASE_URL}:8801/Usermanagement/loginv1`,
+      `${process.env.BASE_URL}:8802/dashboard/getlastdata`,
       {
         method: "POST",
         headers: {
@@ -27,38 +30,32 @@ export default async function handler(req, res) {
           "Access-Control-Allow-Methods": "POST",
           "Access-Control-Allow-Headers":
             "Content-Type, Accept, Origin, X-Requested-With",
-          "Cache-Control": "s-maxage=10",
           tenant: tenant,
           token: process.env.AUTH_TOKEN,
         },
         body: JSON.stringify({
-          userName: userName,
-          password: password,
-          salt: salt,
+          locationid: locationid,
+          lane: lane,
+          status: status,
+          value: value,
+          side: side,
+          start_trancation_date: start_trancation_date,
+          end_trancation_date: end_trancation_date,
+          tenant: tenant,
         }),
       }
     );
 
     if (!response.ok) {
       if (process.env.NODE_ENV === "development") {
-        res
-          .status(response.status)
-          .json({ message: "Status: " + response.status });
+        res.status(401).json({ message: "Status: " + response.status });
       }
       throw new Error("Service Unavailable");
-    } else {
-      res.setHeader(
-        "Set-Cookie",
-        `tenant=${encryptedCookieString}; Path=/; HttpOnly; SameSite=None; Secure=${
-          process.env.NODE_ENV === "production"
-        }; Expires=${Date.now - oneDay}`
-      );
-
-      const data = await response.json();
-      res
-        .status(response.status)
-        .json({ message: "Login successfully", datalogin: data });
     }
+
+    const data = await response.json();
+
+    res.status(200).json({ message: response.statusText, monitoring: data });
   } catch (e) {
     if (process.env.NODE_ENV === "development") {
       console.error(e);
