cc76ee584d1f949e8423c1793f66c80c93731700
Muhammad Hanafi Prasyah <74239181+hanafiprasyah@users.noreply.github.com>
2024-10-01 12:55:33 +0700

    pref(voltage): UX Improvement

--- app/(dashboard)/dashboard/(monitoring)/voltage/page.jsx
+++ app/(dashboard)/dashboard/(monitoring)/voltage/page.jsx
@@ -23,7 +23,7 @@ export default function Voltage() {
   // Dates
   const [currentDate, setCurrentDate] = useState("");
   const [hoursAgo, setHoursAgo] = useState("");
-  // const [differentTime, setDifferentTime] = useState("");
+  const [differentTime, setDifferentTime] = useState("");
 
   // Init the device connection status and signal recipient status
   const [signal, setSignal] = useState(false);
@@ -187,9 +187,22 @@ export default function Voltage() {
       .then((datas) => {
         if (datas) {
           if (selectDev[0] !== null || selectDev[0] !== undefined) {
-            setDataVolt(datas.monitoring["data"]["datavoltages"]);
+            // We will check the difference about last send_date from API and current date
+            const currentDate = new Date();
+            const sendDate =
+              datas.monitoring["data"]["datavoltages"][0].send_date;
 
-            if (dataVolt === null || dataVolt === undefined) {
+            const isoConvSendDate = new Date(sendDate);
+            const diffTime = currentDate - isoConvSendDate;
+            const minutes = Math.floor(diffTime / 60000);
+
+            if (minutes >= 15) {
+              setDataVolt([]);
+            } else {
+              setDataVolt(datas.monitoring["data"]["datavoltages"]);
+            }
+
+            if (dataVolt.length === 0 || dataVolt === undefined) {
               setSignal(false);
               setChannel("Unreachable");
             } else {
@@ -197,11 +210,15 @@ export default function Voltage() {
               setChannel("Stable");
             }
           } else {
-            setDataVolt([]);
+            setSignal(false);
+            setChannel("Unreachable");
           }
         } else {
-          setChannel("Unreachable");
+          setDataVolt([]);
         }
+      })
+      .catch((err) => {
+        throw new Error(err);
       });
   };
 
@@ -218,66 +235,20 @@ export default function Voltage() {
     ([url, localTenant, locationid, start_date]) =>
       fetchVoltageRealtime(url, localTenant, locationid, start_date),
     {
-      refreshInterval: 1000,
+      isPaused: () => (selectDev.length === 0 ? true : false),
+      refreshInterval: 500,
       dedupingInterval: 500,
       revalidateIfStale: true,
       revalidateOnFocus: true,
       revalidateOnMount: true,
       revalidateOnReconnect: true,
+      refreshWhenHidden: false,
+      refreshWhenOffline: false,
       errorRetryInterval: 1000,
       errorRetryCount: 10,
       shouldRetryOnError: true,
     }
   );
-
-  // TODO: Get current datetime, this will be mounted at the first time
-  useEffect(() => {
-    const dateIns = new Date();
-
-    // const isoDate = "2024-09-13T11:30:54";
-    // const isoConvDate = new Date(isoDate);
-
-    // Get current date time
-    const getFormatedCurrentDate = `${dateIns.getFullYear()}-${(
-      dateIns.getMonth() + 1
-    )
-      .toString()
-      .padStart(2, "0")}-${dateIns.getDate().toLocaleString("en-US", {
-      minimumIntegerDigits: 2,
-    })} ${dateIns.getHours()}:${dateIns
-      .getMinutes()
-      .toString()
-      .padStart(2, "0")}:${dateIns.getSeconds().toString().padStart(2, "0")}`;
-
-    // Get -1 hour of current date time
-    const getHoursAgo = `${dateIns.getFullYear()}-${(dateIns.getMonth() + 1)
-      .toString()
-      .padStart(2, "0")}-${dateIns.getDate().toLocaleString("en-US", {
-      minimumIntegerDigits: 2,
-    })} ${dateIns.getHours() - 1}:${dateIns
-      .getMinutes()
-      .toString()
-      .padStart(2, "0")}:${dateIns.getSeconds().toString().padStart(2, "0")}`;
-
-    // const diffTime = dateIns - isoConvDate;
-    // const minutes = Math.floor((diffTime % 3600000) / 60000);
-
-    if (getFormatedCurrentDate.startsWith("202")) {
-      setCurrentDate(getFormatedCurrentDate);
-      setHoursAgo(getHoursAgo);
-      // setDifferentTime(diffTime);
-    }
-
-    // if (process.env.NODE_ENV === "development") {
-    //   console.log(
-    //     "Current date: " +
-    //       getFormatedCurrentDate +
-    //       "| 1 hours ago: " +
-    //       getHoursAgo
-    //   );
-    //   console.log("Different time: " + minutes);
-    // }
-  }, []);
 
   // TODO: Get default site and location
   useEffect(() => {
@@ -319,14 +290,12 @@ export default function Voltage() {
             "",
             "",
             "",
-            selectLoc.length === 0
-              ? "0"
-              : JSON.stringify(selectLoc[0]).toString(),
+            selectLoc.length === 0 ? "0" : JSON.stringify(selectLoc[0]),
             "2023-01-01 00:00:00",
             "2024-12-30 23:59:00"
           ).then((dataLocation) => {
             // if (process.env.NODE_ENV === "development") {
-            //   console.log(dataLocation.loc["data"]);
+            //   console.log("fetchDevice: " + dataLocation.loc["data"]);
             // }
 
             if (dataLocation.message == "OK") {
@@ -336,42 +305,6 @@ export default function Voltage() {
               if (selectDev.length === 0) {
                 setSelectDev([firstIndexDev["code"], firstIndexDev["name"]]);
               }
-
-              /**
-               * SCHEMA:
-               * Set signal true
-               * CONDITION:
-               * If send_date from REST API depend on locationid
-               * less than 15 minutes take time by current/now format
-               * Value send_date is using ISO 8206 format
-               * then parse to LocaleString to get the value formatted
-               * then compare between the both of value
-               */
-
-              // delay signal and channel status by 100ms after connection ready
-              setTimeout(() => {
-                setSignal(true);
-                setChannel("Validate data..");
-              }, 100);
-
-              // if (process.env.NODE_ENV === "development") {
-              //   console.log(hoursAgo);
-              //   console.log(currentDate);
-              // }
-
-              // READY TO FLIGHT for Spline Chart -> !
-              // fetchVoltage(
-              //   "alif",
-              //   "102",
-              //   "",
-              //   "",
-              //   "",
-              //   "",
-              //   hoursAgo,
-              //   currentDate
-              // ).then((data) => {
-              //   console.log(data);
-              // });
             } else {
               setSignal(false);
               setChannel("Unreachable");
@@ -441,7 +374,7 @@ export default function Voltage() {
           </span>
           {/* Paragraph */}
           <h1 className="text-xl font-semibold tracking-tight text-gray-800 dark:text-neutral-200">
-            Server is under maintenance
+            An unexpected error occurred on our server.
           </h1>
           <p className="text-sm text-gray-500 dark:text-neutral-500">
             We cannot provide you with the latest data at this time, please wait
@@ -457,11 +390,6 @@ export default function Voltage() {
     <div id="voltage-template" className="grid grid-cols-1 gap-0 mt-2">
       {/* Page Heading */}
       <div className="px-2 pb-2 md:px-1 sm:pb-4">
-        {/* Title */}
-        {/* <h4 className="pt-2 pb-4 text-3xl font-semibold text-gray-800 lg:text-4xl md:pb-4 md:pt-0 dark:text-neutral-200">
-          Voltage
-        </h4> */}
-        {/* End Title */}
         <div className="-ms-[5px] flex justify-between items-center gap-1 sm:gap-2">
           <div className="flex flex-wrap items-center gap-1 sm:gap-2">
             {/* Select Location */}
@@ -531,7 +459,6 @@ export default function Voltage() {
               </div>
             </div>
             {/* End Select Location */}
-
             {/* Select Device */}
             <div className="relative ps-0.5 sm:ps-2 before:block before:absolute before:top-1/2 before:-start-px before:w-px before:h-4 before:bg-gray-300 before:-translate-y-1/2 dark:before:bg-neutral-700">
               <div
