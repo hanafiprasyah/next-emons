ecaab5b0ddacb7841f7a26dac3b0e0a9ce3b14a1
Muhammad Hanafi Prasyah <74239181+hanafiprasyah@users.noreply.github.com>
2024-10-16 17:53:24 +0700

    fix(monitoring): Change throttling interval to 3 sec avoid mismatch data from BE

--- app/(dashboard)/dashboard/(monitoring)/voltage/page.jsx
+++ app/(dashboard)/dashboard/(monitoring)/voltage/page.jsx
@@ -214,12 +214,13 @@ export default function Voltage() {
         (hoursAgo == "" && hoursAgo == undefined)
           ? true
           : false,
-      refreshInterval: 500,
+      refreshInterval: 1000,
+      focusThrottleInterval: 3000,
       errorRetryInterval: 1000,
       errorRetryCount: 10,
       shouldRetryOnError: true,
       keepPreviousData: true,
-      loadingTimeout: 5000,
+      loadingTimeout: 6000,
       onLoadingSlow: () => {
         setChannel("Unstable network, please wait..");
       },
