f66d95ffa6280aecef11658096c5c812fa2024e1
Muhammad Hanafi Prasyah <74239181+hanafiprasyah@users.noreply.github.com>
2024-10-30 11:35:17 +0700

    fix(ground): Wrong usage of data null checker, fix hidden device while data is undefined and fix single phase or three phase parameter usages

--- app/(dashboard)/dashboard/(monitoring)/ground/page.jsx
+++ app/(dashboard)/dashboard/(monitoring)/ground/page.jsx
@@ -190,17 +190,21 @@ export default function Grounding() {
         if (selectDev.length !== 0) {
           setSignal(true);
           // Check if data ground length is null
-          if (data.monitoring["data"]["datagrounds"][0] === null) {
+          if (
+            data.monitoring["data"]["datagrounds"][0] === undefined ||
+            data.monitoring["data"]["datagrounds"][0] === null
+          ) {
             // Give signal to offline, and set channel to unreachable
             setOnLoading(false);
             setSignal(false);
-            setChannel("Unreachable");
+            setChannel("Device unreachable");
           } else {
-            setSignal(true);
             // We will check the difference about last send_date from API and current date from NOW()
+            setSignal(true);
             const currentDate = new Date();
             const sendDate =
               data.monitoring["data"]["datagrounds"][0].send_date;
+
             // format the send_date value
             const isoConvSendDate = new Date(sendDate);
             // count the diff
@@ -209,7 +213,7 @@ export default function Grounding() {
             const minutes = Math.floor(diffTime / 60000);
 
             // Set offline status if the diff time more than 5 minutes from NOW()
-            if (minutes >= 1) {
+            if (minutes >= process.env.MAX_LAST_TRIGGER_MINUTE) {
               setOnLoading(false);
               setSignal(false);
               setChannel("Device signal interference");
@@ -231,6 +235,8 @@ export default function Grounding() {
       // If response message is not OK
       else {
         setSignal(false);
+        setOnLoading(false);
+        setChannel("Server error");
         if (process.env.NODE_ENV === "development") {
           console.log(
             "Error in fetchGroundRealtime: Response Message is Not OK"
@@ -238,6 +244,9 @@ export default function Grounding() {
         }
       }
     } catch (err) {
+      setSignal(false);
+      setOnLoading(false);
+      setChannel("Error while fetch data");
       if (process.env.NODE_ENV === "development") {
         console.log("Error in fetchGroundRealtime: ", err);
       }
@@ -255,21 +264,8 @@ export default function Grounding() {
   // SWR
   const { data, isLoading, error } = useSWR(
     ["/api/monitoring/getmonitoring", localTenant, selectDev[0], hoursAgo],
-    async ([url, localTenant, locationid, start_date]) => {
-      try {
-        return await fetchGroundRealtime(
-          url,
-          localTenant,
-          locationid,
-          start_date
-        );
-      } catch (err) {
-        if (process.env.NODE_ENV === "development") {
-          console.log("Error SWR Fetch data: ", err);
-        }
-        throw err;
-      }
-    },
+    ([url, localTenant, locationid, start_date]) =>
+      fetchGroundRealtime(url, localTenant, locationid, start_date),
     {
       isPaused: () =>
         selectDev.length === 0 ||
@@ -277,7 +273,7 @@ export default function Grounding() {
         (hoursAgo == "" && hoursAgo == undefined)
           ? true
           : false,
-      refreshInterval: 3500,
+      refreshInterval: 3000,
       revalidateOnFocus: false,
       loadingTimeout: 6000,
       onLoadingSlow: () => {
@@ -515,6 +511,7 @@ export default function Grounding() {
           <div className="flex flex-wrap items-center gap-1 sm:gap-2">
             {/* Select Location */}
             <div className="relative inline-block">
+              <h2 className="pb-2 text-xs ps-1">Location:</h2>
               <div
                 className={`relative inline-flex hs-dropdown hs-dropdown-example ${
                   selectLoc.length === null ? "pointer-events-none" : null
@@ -581,7 +578,8 @@ export default function Grounding() {
             {/* End Select Location */}
 
             {/* Select Device */}
-            <div className="relative ps-0.5 sm:ps-2 before:block before:absolute before:top-1/2 before:-start-px before:w-px before:h-4 before:bg-gray-300 before:-translate-y-1/2 dark:before:bg-neutral-700">
+            <div className="relative ps-0.5 sm:ps-2 before:block before:absolute before:top-1/2 before:-start-px before:w-px before:bg-gray-300 before:-translate-y-1/2 dark:before:bg-neutral-700">
+              <h2 className="pb-2 text-xs ps-1">Device:</h2>
               <div
                 className={`relative inline-flex hs-dropdown hs-dropdown-example ${
                   selectDev.length === null || !showDev
@@ -694,89 +692,97 @@ export default function Grounding() {
               {!onLoading ? (
                 <>
                   <div className="w-full h-full md:w-1/2">
-                    {signal ? (
-                      data.map((item, index) => {
-                        if (item.location_id === selectDev[0]) {
+                    {data !== undefined ? (
+                      data?.map((item, index) => {
+                        if (signal && item.location_id === selectDev[0]) {
                           return (
                             <RadialDynamicGauge
                               id={"ground-input"}
                               key={"ground-input"}
                               alt={"Ground"}
                               title="Input"
-                              value={
-                                item.location_id === selectDev[0]
-                                  ? item.voltage_input
-                                  : 0
-                              }
+                              value={!error ? item.voltage_input : 0}
                             />
                           );
                         }
-                        return null;
+                        return (
+                          <div key={`groundInput${index}`}>
+                            <span className="inline-flex items-center px-2 py-1 my-4 text-xs text-gray-800 bg-gray-100 rounded-full gap-x-1 dark:bg-neutral-500/20 dark:text-neutral-400">
+                              <svg
+                                className="shrink-0 size-3"
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="24"
+                                height="24"
+                                viewBox="0 0 24 24"
+                                fill="none"
+                                stroke="currentColor"
+                                strokeWidth="2"
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                              >
+                                <path d="M18.36 6.64a9 9 0 1 1-12.73 0"></path>
+                                <line x1="12" x2="12" y1="2" y2="12"></line>
+                              </svg>
+                              Device is not connected
+                            </span>
+                          </div>
+                        );
                       })
                     ) : (
-                      <div id={"groundInput"}>
-                        <span className="inline-flex items-center px-2 py-1 my-4 text-xs text-gray-800 bg-gray-100 rounded-full gap-x-1 dark:bg-neutral-500/20 dark:text-neutral-400">
-                          <svg
-                            className="shrink-0 size-3"
-                            xmlns="http://www.w3.org/2000/svg"
-                            width="24"
-                            height="24"
-                            viewBox="0 0 24 24"
-                            fill="none"
-                            stroke="currentColor"
-                            strokeWidth="2"
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                          >
-                            <path d="M18.36 6.64a9 9 0 1 1-12.73 0"></path>
-                            <line x1="12" x2="12" y1="2" y2="12"></line>
-                          </svg>
-                          Device is not connected
-                        </span>
-                      </div>
+                      <RadialDynamicGauge
+                        id={"ground-input"}
+                        key={"ground-input"}
+                        alt={"Ground"}
+                        title="Input"
+                        value={0}
+                      />
                     )}
                   </div>
                   <div className="w-full h-full md:w-1/2">
-                    {signal ? (
-                      data.map((item, index) => {
-                        if (item.location_id === selectDev[0]) {
+                    {data !== undefined ? (
+                      data?.map((item, index) => {
+                        if (signal && item.location_id === selectDev[0]) {
                           return (
                             <RadialDynamicGauge
                               id={"ground-output"}
                               key={"ground-output"}
                               alt={"Ground"}
                               title="Output"
-                              value={
-                                item.location_id === selectDev[0]
-                                  ? item.voltage_output
-                                  : 0
-                              }
+                              value={!error ? item.voltage_output : 0}
                             />
                           );
                         }
-                        return null;
+                        return (
+                          <div key={`groundOutput${index}`}>
+                            <span className="inline-flex items-center px-2 py-1 my-4 text-xs text-gray-800 bg-gray-100 rounded-full gap-x-1 dark:bg-neutral-500/20 dark:text-neutral-400">
+                              <svg
+                                className="shrink-0 size-3"
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="24"
+                                height="24"
+                                viewBox="0 0 24 24"
+                                fill="none"
+                                stroke="currentColor"
+                                strokeWidth="2"
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                              >
+                                <path d="M18.36 6.64a9 9 0 1 1-12.73 0"></path>
+                                <line x1="12" x2="12" y1="2" y2="12"></line>
+                              </svg>
+                              Device is not connected
+                            </span>
+                          </div>
+                        );
                       })
                     ) : (
-                      <div id={"groundOutput"}>
-                        <span className="inline-flex items-center px-2 py-1 my-4 text-xs text-gray-800 bg-gray-100 rounded-full gap-x-1 dark:bg-neutral-500/20 dark:text-neutral-400">
-                          <svg
-                            className="shrink-0 size-3"
-                            xmlns="http://www.w3.org/2000/svg"
-                            width="24"
-                            height="24"
-                            viewBox="0 0 24 24"
-                            fill="none"
-                            stroke="currentColor"
-                            strokeWidth="2"
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                          >
-                            <path d="M18.36 6.64a9 9 0 1 1-12.73 0"></path>
-                            <line x1="12" x2="12" y1="2" y2="12"></line>
-                          </svg>
-                          Device is not connected
-                        </span>
-                      </div>
+                      <RadialDynamicGauge
+                        id={"ground-output"}
+                        key={"ground-output"}
+                        alt={"Ground"}
+                        title="Output"
+                        value={0}
+                      />
                     )}
                   </div>
                 </>
